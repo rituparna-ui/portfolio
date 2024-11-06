06c9d3498cdef2939fe1d960c1a6b4624e2900a3
Rituparna W <rituparnaw16@gmail.com>
2024-11-07 00:39:50 +0530

    feat(home): Complete home page

---
+++ public/assets/typed.js
@@ -0,0 +1,15 @@
+new Typed(".typed", {
+  strings: [
+    "a Full Stack Developer.",
+    "an Angular Developer.",
+    "a Flutter Developer.",
+    "a React Developer.",
+    "a Backend Developer.",
+    "a ME(F/A/R)N Stack Developer.",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  showCursor: true,
+  cursorChar: "/",
+  loop: true,
+});
