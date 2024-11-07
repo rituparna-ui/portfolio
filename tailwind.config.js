4be9cba10f1349ea57d5706afd4858d2add4d671
Rituparna W <rituparnaw16@gmail.com>
2024-11-07 13:20:03 +0530

    feat(projects): add project sections. TODO: add carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-1": "#1E2328",
+        "dark-2": "#323C46",
+      },
+    },
   },
   plugins: [],
 };
